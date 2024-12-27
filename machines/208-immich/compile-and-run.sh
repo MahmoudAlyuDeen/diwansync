fffdd8f59ccd000c41ad95e95d8fa2c979ed5084
root <root@proxmox.speedport.ip>
2024-12-27 23:46:36 +0100

    - 208/221/222-immich: move merged-config.yml from shared to private config.

--- machines/208-immich/compile-and-run.sh
+++ machines/208-immich/compile-and-run.sh
@@ -2,7 +2,7 @@ set -a # export all variables
 source .env
 set +a # undo the set -a
 
-envsubst < "config.yml" > "merged-config.yml"
+envsubst < "config.yml" > "immich/merged-config.yml"
 
 docker compose up -d --force-recreate
 
