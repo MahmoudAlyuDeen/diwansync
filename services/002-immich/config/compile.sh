016b92ef33a476a647c939ff18c98965143eee06
MahmoudAlyuDeen <mahmoudalyudeen@gmail.com>
2026-09-12 20:30:19 +0300

    002-immich: unmodularize config structure + authelia oauth.

--- services/002-immich/config/compile.sh
+++ services/002-immich/config/compile.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Compiles Immich configuration template with env variables:
-# - config templates:
-#   - <repo>/services/002-immich/config/config-base.yml
-#   - <repo>/services/002-immich/config/config-oauth.yml
-#   - <repo>/services/002-immich/config/config-smtp.yml
-# - env variables: <storage>/env/002-immich.env.
-# - config templates and this compile script are bind mounted under ./config:/config
-# - writes into /compiled/configuration.yml, runs inside the container.
+# - config template: <repo>/services/002-immich/config/config.yml
+# - env variables: <storage>/env/002-immich.env and <storage>/env/004-dyngress.env.
+# - the config template and this compile script are bind mounted under ./config:/config
+# - writes into /compiled/config.yml, runs inside the container.
 # - the compiled configuration is ephemeral, it recompiles on every container start.
 set -euo pipefail
 
@@ -19,17 +16,8 @@ parse_template() {
     done
 }
 
-echo "Compiling base config..."
-parse_template < /config/config-base.yml > /compiled/config.yml
-
-if [ "${ENABLE_OAUTH:-false}" = "true" ]; then
-    echo "Compiling OAuth configuration..."
-    parse_template < /config/config-oauth.yml >> /compiled/config.yml
-fi
-
-if [ "${ENABLE_SMTP:-false}" = "true" ]; then
-    echo "Compiling SMTP configuration..."
-    parse_template < /config/config-smtp.yml >> /compiled/config.yml
-fi
+echo "Compiling Immich configuration..."
+parse_template < /config/config.yml > /compiled/config.yml
 
 echo "Configuration successfully compiled at /compiled/config.yml!"
+cat /compiled/config.yml
